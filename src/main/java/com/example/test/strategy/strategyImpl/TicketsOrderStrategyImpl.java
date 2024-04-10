ab5b901ea3a3a343944f55461ef8181fce955783
chenjw <chixe9233@163.com>
2024-04-10 09:37:34 +0800

    init

---
+++ src/main/java/com/example/test/strategy/strategyImpl/TicketsOrderStrategyImpl.java
@@ -0,0 +1,17 @@
+package com.example.test.strategy.strategyImpl;
+
+import com.example.test.enums.OrderStatusEnum;
+import com.example.test.strategy.OrderStrategy;
+/**
+ * @Author chenjw
+ * @Email chixe9233@163.com
+ * @Date  2024/4/10 9:37
+ * @Version 1.0
+ * @Desc some description
+*/
+public class TicketsOrderStrategyImpl  implements OrderStrategy {
+    @Override
+    public void process(OrderStatusEnum orderStatusEnum) {
+
+    }
+}
