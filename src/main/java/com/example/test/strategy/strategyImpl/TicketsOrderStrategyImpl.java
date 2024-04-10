0af6902c18e5a6f21921ece748b5f3b2c0333643
chenjw <chixe9233@163.com>
2024-04-10 09:43:03 +0800

    init

--- src/main/java/com/example/test/strategy/strategyImpl/TicketsOrderStrategyImpl.java
+++ src/main/java/com/example/test/strategy/strategyImpl/TicketsOrderStrategyImpl.java
@@ -12,6 +12,19 @@ import com.example.test.strategy.OrderStrategy;
 public class TicketsOrderStrategyImpl  implements OrderStrategy {
     @Override
     public void process(OrderStatusEnum orderStatusEnum) {
-
+//        long payId = -1;
+//        for (int i = 0; i < 3; i++) { // 如果失败有3次重试
+//            // 信用支付
+//            payId = payService.payOfCredit(orderInfo);
+//            // payId >0 代表支付成功
+//            if (payId > 0) {
+//                break;
+//            }
+//        }
+//        // 如果支付失败关闭订单
+//        if(payId < 0) {
+//            payService.notPayedToClose(orderInfo);
+//        }
+//
     }
 }
